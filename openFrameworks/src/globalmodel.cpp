dc1b4966e03fb7a3cdcb07b1ea0076a8b0765cfd
magiwanders <magi.wanders@gmail.com>
2022-01-04 17:14:02 +0100

    Ora il FRONTEND riceve osc (ma python non lo manda ancora)

---
+++ openFrameworks/src/globalmodel.cpp
@@ -0,0 +1,6 @@
+#include "globalmodel.h"
+
+GlobalModel::GlobalModel()
+{
+
+}
---
+++ openFrameworks/src/globalmodel.h
@@ -0,0 +1,20 @@
+#ifndef GLOBALMODEL_H
+#define GLOBALMODEL_H
+
+//using namespace std;
+#include <map>
+#include <string>
+#include <any>
+
+#include <pose.h>
+
+class GlobalModel
+{
+public:
+    GlobalModel();
+    float blend=0.0; // 0 -> Full pose | 1 -> Full other_pose
+    Pose pose;
+    Pose other_pose;
+};
+
+#endif // GLOBALMODEL_H
