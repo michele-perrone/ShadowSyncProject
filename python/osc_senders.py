51dd9bdde1c4b31bf2b4763a34a06474a8a4e623
davide <davide.lista@mail.polimi.it>
2022-01-05 22:35:25 +0100

    update landmarks and pose esitmation

--- python/osc_senders.py
+++ python/osc_senders.py
@@ -1,14 +1,12 @@
 def pose_sender(client, pose):
     #client.send_message("/pose", [pose['left_shoulder'], pose['right_shoulder']])   # Send float message
     client.send_message("/pose", [
-        pose['left_shoulder'][0],
-        pose['left_shoulder'][1],
-        pose['right_shoulder'][0],
-        pose['right_shoulder'][1],
-        pose['left_hip'][0],
-        pose['left_hip'][1],
-        pose['right_hip'][0],
-        pose['right_hip'][1],
-        pose['body'][0],
-        pose['body'][1],
+        pose['body']['left_shoulder'][0],
+        pose['body']['left_shoulder'][1],
+        pose['body']['right_shoulder'][0],
+        pose['body']['right_shoulder'][1],
+        pose['body']['left_hip'][0],
+        pose['body']['left_hip'][1],
+        pose['body']['right_hip'][0],
+        pose['body']['right_hip'][1],
         ])
--- python/pose_estimation.py
+++ python/pose_estimation.py
@@ -10,21 +10,21 @@ def init_pose_estimation():
     # poseLandmarksArray = ["nose", "left_eye_inner", "left_eye", "left_eye_outer", "right_eye_inner", "right_eye", "right_eye_outer", "left_ear", "right_ear", "mouth_left", "mouth_right", "left_shoulder", "right_shoulder", "left_elbow", "right_elbow", "left_wrist", "right_wrist", "left_pinky", "right_pinky", "left_index", "right_index", "left_thumb", "right_thumb", "left_hip", "right_hip", "left_knee", "right_knee", "left_ankle", "right_ankle", "left_heel", "right_heel", "left_foot_index", "right_foot_index"]
 
     # obtain video/webcam
-    cap = cv2.VideoCapture(os.path.join(os.getcwd(), 'Videos', "robot_dance.mp4"))
-    #cap = cv2.VideoCapture(0)
+    # cap = cv2.VideoCapture(os.path.join(os.getcwd(), 'Videos', "robot_dance.mp4"))
+    cap = cv2.VideoCapture(0)
 
     mpDraw = mp.solutions.drawing_utils
     mpPose = mp.solutions.pose
     pose_cv = mpPose.Pose()  # with default params for detection and tracking tolerance (until detection confidence is high enough, it keeps tracking)
 
-    # os_name = platform.system()
-    # if "Windows" in os_name:
-    #     data = open('python\data\landmark.json')
-    # else:
-    #     data = open('./data/landmark.json')
+    os_name = platform.system()
+    if "Windows" in os_name:
+        data = open('python\data\landmark.json')
+    else:
+        data = open('./data/landmark.json')
 
-    path = os.path.join(os.getcwd(), 'python', 'data', 'landmark.json')
-    data = open(path)
+    # path = os.path.join(os.getcwd(), 'python', 'data', 'landmark.json')
+    # data = open(path)
     pose = json.load(data)
     poseLandmarksArray = [x.upper() for x in pose]
 
@@ -36,6 +36,7 @@ def init_pose_estimation():
     if not cap.isOpened():
         print("Error opening video file")
     return cap, mpDraw, mpPose, pose_cv, pose, poseLandmarksArray
+
 
 def get_body_position(img, mpDraw, mpPose, pose_cv, pose, poseLandmarksArray):
     imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # conversion of the acquired img in RGB scale
@@ -49,19 +50,23 @@ def get_body_position(img, mpDraw, mpPose, pose_cv, pose, poseLandmarksArray):
             cv2.circle(img, (cx, cy), 5, (155, 155, 0))  # superimpose a circle to the landmarks
 
         for i in pose:
-            if i != "body":
-                upperI = i.upper()
-                pose[i] = [results.pose_landmarks.landmark[mpPose.PoseLandmark[upperI]].x,
-                            results.pose_landmarks.landmark[mpPose.PoseLandmark[upperI]].y,
-                            results.pose_landmarks.landmark[mpPose.PoseLandmark[upperI]].z]
-                # if i == "right_thumb":
-                #     print("z coord: ", pose[i][2]) #testing z coord estimation with right hand
-            else:
-                pose[i] = [int((pose['left_shoulder'][0] + pose['right_shoulder'][0] + pose['left_hip'][0] + pose['right_hip'][0])) / 4,
-                            int((pose['left_shoulder'][1] + pose['right_shoulder'][1] + pose['left_hip'][1] + pose['right_hip'][1])) / 4]
+            centroid = [0, 0, 0]
+            length = len(pose[i]) - 3
+            for j in pose[i]:
+                if not j.startswith('_'):
+                    upperJ = j.upper()
+                    pose[i][j] = [results.pose_landmarks.landmark[mpPose.PoseLandmark[upperJ]].x,
+                                  results.pose_landmarks.landmark[mpPose.PoseLandmark[upperJ]].y,
+                                  results.pose_landmarks.landmark[mpPose.PoseLandmark[upperJ]].z]
 
-        cv2.circle(img, (int(pose["body"][0] * w), int(pose["body"][1] * h)), 8, (155, 155, 222),
-                   cv2.FILLED)  # superimpose a circle to the landmarks
+                    if pose[i][j] == [0, 0, 0]:
+                        break
+                    centroid = [x + y for x, y in zip(centroid, pose[i][j])]
+
+            centroid = [x / length for x in centroid]
+
+            pose[i]['_centroid'] = centroid
+            print(i, pose[i]['_centroid'])
 
     if cv2.waitKey(10) & 0xFF == ord('q'):
         return
