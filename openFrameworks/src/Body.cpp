c060747cb06fcd5122a25d0692093f66629c98d5
Michele Perrone <perrone.michele@outlook.com>
2022-01-11 13:00:29 +0100

    OFX: Add a TRASL_VECTOR and a SCALE_VECTOR for the Body These vectors are applied on the normalized coordinates. The order of the transformations is: TRASL_VECTOR -> SCALE_VECTOR -> SCALE_FACTOR

--- openFrameworks/src/Body.cpp
+++ openFrameworks/src/Body.cpp
@@ -3,6 +3,9 @@
 #define P_LIFESPAN 200
 #define P_RADIUS 1
 #define SCALE_FACTOR 150
+#define TRASL_VECTOR {0, -1.5, -0.5}
+#define SCALE_VECTOR {1, -1, -1}
+
 
 
 void Body::setup(Pose* pose_body)
@@ -76,46 +79,50 @@ void Body::move_centroid()
 void Body::move_junctions()
 {
     //once the pose elems are updated via osc, we set the body junctions accordingly
+    glm::vec3 scale_vec(SCALE_VECTOR);
+    scale_vec *= SCALE_FACTOR;
+    glm::vec3 trasl_vec(TRASL_VECTOR);
+
     //face
-    body_junctions[0].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->nose));
-    body_junctions[1].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_eye_inner));
-    body_junctions[2].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_eye));
-    body_junctions[3].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_eye_outer));
-    body_junctions[4].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_eye_inner));
-    body_junctions[5].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_eye));
-    body_junctions[6].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_eye_outer));
-    body_junctions[7].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_ear));
-    body_junctions[8].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_ear));
-    body_junctions[9].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->mouth_left));
-    body_junctions[10].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->mouth_right));
+    body_junctions[0].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->nose)));
+    body_junctions[1].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_eye_inner)));
+    body_junctions[2].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_eye)));
+    body_junctions[3].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_eye_outer)));
+    body_junctions[4].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_eye_inner)));
+    body_junctions[5].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_eye)));
+    body_junctions[6].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_eye_outer)));
+    body_junctions[7].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_ear)));
+    body_junctions[8].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_ear)));
+    body_junctions[9].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->mouth_left)));
+    body_junctions[10].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->mouth_right)));
     //shoulder
-    body_junctions[11].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_shoulder));
-    body_junctions[12].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_shoulder));
+    body_junctions[11].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_shoulder)));
+    body_junctions[12].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_shoulder)));
     //hip
-    body_junctions[13].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_hip));
-    body_junctions[14].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_hip));
+    body_junctions[13].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_hip)));
+    body_junctions[14].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_hip)));
     //left arm
-    body_junctions[15].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_pinky));
-    body_junctions[16].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_index));
-    body_junctions[17].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_thumb));
-    body_junctions[18].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_elbow));
-    body_junctions[19].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_wrist));
+    body_junctions[15].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_pinky)));
+    body_junctions[16].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_index)));
+    body_junctions[17].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_thumb)));
+    body_junctions[18].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_elbow)));
+    body_junctions[19].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_wrist)));
     //right harm
-    body_junctions[20].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_pinky));
-    body_junctions[21].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_index));
-    body_junctions[22].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_thumb));
-    body_junctions[23].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_elbow));
-    body_junctions[24].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_wrist));
+    body_junctions[20].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_pinky)));
+    body_junctions[21].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_index)));
+    body_junctions[22].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_thumb)));
+    body_junctions[23].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_elbow)));
+    body_junctions[24].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_wrist)));
     //left leg
-    body_junctions[25].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_heel));
-    body_junctions[26].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_foot_index));
-    body_junctions[27].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_knee));
-    body_junctions[28].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->left_ankle));
+    body_junctions[25].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_heel)));
+    body_junctions[26].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_foot_index)));
+    body_junctions[27].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_knee)));
+    body_junctions[28].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->left_ankle)));
     //right leg
-    body_junctions[29].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_heel));
-    body_junctions[30].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_foot_index));
-    body_junctions[31].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_knee));
-    body_junctions[32].setGlobalPosition(SCALE_FACTOR * glm::make_vec3(pose->right_ankle));
+    body_junctions[29].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_heel)));
+    body_junctions[30].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_foot_index)));
+    body_junctions[31].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_knee)));
+    body_junctions[32].setGlobalPosition(scale_vec * (trasl_vec +glm::make_vec3(pose->right_ankle)));
 
 }
 
